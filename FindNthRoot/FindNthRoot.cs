a2d15c895e3b3b1196d560ffa3a6d521206272f6
Maralov Kemal <kemo181098@gmail.com>
2020-10-23 16:39:55 +0600

    Add project files.

---
+++ FindNthRoot/FindNthRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindNthRootAlgorithm
+{
+    public class FindNthRoot
+    {
+        private Random _random = new Random(); // uses random to guess inital value
+        /// <summary>
+        /// The method applies Newton's method to find Nth root of an input number via given precision 
+        /// </summary>
+        /// <param name="inputNumber">your input number must be greater than zero</param>
+        /// <param name="inputDegree">your input degree has to be greater than zero and Odd!</param>
+        /// <param name="inputPrecision">better keep this values between zero and one</param>
+        /// <returns>this method returns your desired root</returns>
+        public double GetRoot(double inputNumber, int inputDegree, double inputPrecision)
+        {
+            if ((inputNumber < 0 && inputDegree % 2 == 0) ||
+                 inputDegree <= 0 || inputPrecision >= 1  || 
+                 inputPrecision < 0) 
+            throw new ArgumentOutOfRangeException("Sorry, your input values are not valid!");
+
+            var result = 0.0d;
+            var previousResult = _random.Next(10); // generates a random number between 0 and 9
+            var deltaDifferenceOfRoots = Double.MaxValue; // gonna help to get the difference of two roots
+           
+            // iterate till we get the required precision
+            while(deltaDifferenceOfRoots > inputPrecision)
+            {
+                result = ((inputDegree - 1.0) * previousResult + inputNumber / Math.Pow(previousResult, inputDegree - 1)) / (double)inputDegree;
+                deltaDifferenceOfRoots = Math.Abs(result - previousResult);
+                previousResult = (int)result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestFindNthRootAlgorithm/TestFindNthRoot.cs
@@ -0,0 +1,111 @@
+﻿using FindNthRootAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestFindNthRootAlgorithm
+{
+    /*
+     * Реализовать алгоритм FindNthRoot, позволяющий вычислять
+     * корень n-ой степени ( n ∈ N ) из вещественного числа 
+     * а методом Ньютона с заданной точностью. 
+     * Разработать модульные тесты (NUnit и (или) MS Unit Test) 
+     * для тестирования метода.
+     */
+
+    [TestClass]
+    public class TestFindNthRoot
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_1_FindNthRootMethod_Number_Degree_Precision_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = -0.01;
+            var inputDegree = 2;
+            var inputPrecision = 0.0001;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);         
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_2_FindNthRootMethod_Number_Degree_Precision_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = 0.001;
+            var inputDegree = -2;
+            var inputPrecision = 0.0001;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_3_FindNthRootMethod_Number_Degree_Precision_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = 0.01;
+            var inputDegree = 2;
+            var inputPrecision = -1;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);
+        }
+
+        [TestMethod]
+        public void Test_4_FindNthRootMethod()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = 1;
+            var inputDegree = 5;
+            var inputPrecision = 0.0001;
+            var expectedResult = 1;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_5_FindNthRootMethod()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = 8;
+            var inputDegree = 3;
+            var inputPrecision = 0.0001;
+            var expectedResult = 2;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_6_FindNthRootMethod()
+        {
+            // Arrange
+            var find = new FindNthRoot();
+            var inputNumber = 8;
+            var inputDegree = 3;
+            var inputPrecision = 0.0001;
+            var expectedResult = 2;
+
+            // Act
+            var actualResult = find.GetRoot(inputNumber, inputDegree, inputPrecision);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
